0f6b84b1001daf0989a00888764465ac1b19c51a
Angelo <angelosansone97@gmail.com>
2020-04-20 11:50:03 +0200

    Aggiunta memorizzazione degli slang, delle emoticon e delle stopwords

--- loadMongoDBResources.py
+++ loadMongoDBResources.py
@@ -1,10 +1,56 @@
 import pymongo
 import os
+from slang_emojii_emoticon_stopwords import slang, stop_words, pos_emoticons, neg_emoticons, EmojiPos, EmojiNeg, \
+    OthersEmoji
+
+# import nltk
+# nltk.download()
+# from nltk.corpus import stopwords
+# sw = stopwords.words("english")
 
 numberOfClusters = 2
 client = pymongo.MongoClient('localhost', 27017)
 path_tweet = 'Risorse\Twitter messaggi'
 path_negative_word = "Risorse\elenco-parole-che-negano-parole-successive.txt"
+
+def load_emojii_emoticon():
+    emojii_emoticon_documents = []
+    for emoticon in pos_emoticons:
+        emojii_emoticon_documents.append({"Code": emoticon, "Polarity": 1})
+    for emoticon in neg_emoticons:
+        emojii_emoticon_documents.append({"Code": emoticon, "Polarity": -1})
+    for emoticon in EmojiPos:
+        emojii_emoticon_documents.append({"Code": emoticon, "Polarity": 1})
+    for emoticon in EmojiNeg:
+        emojii_emoticon_documents.append({"Code": emoticon, "Polarity": -1})
+    for emoticon in OthersEmoji:
+        emojii_emoticon_documents.append({"Code": emoticon, "Polarity": 0})
+    for i in range(1, numberOfClusters + 1):
+        print('cluster_' + str(i))
+        db = client['cluster_' + str(i)]
+        col = db.Emoticon
+        col.insert_many(emojii_emoticon_documents)
+
+def load_stopwords():
+    stopwords_documents = []
+    for stop_word in stop_words:
+        stopwords_documents.append({"Word": stop_word})
+    for i in range(1, numberOfClusters + 1):
+        print('cluster_' + str(i))
+        db = client['cluster_' + str(i)]
+        col = db.StopWord
+        col.insert_many(stopwords_documents)
+
+def load_slang():
+    slang_documents = []
+    for key in slang:
+        slang_documents.append({"Slang": key, "Traduction": slang[key]})
+
+    for i in range(1, numberOfClusters + 1):
+        print('cluster_' + str(i))
+        db = client['cluster_' + str(i)]
+        col = db.Slang
+        col.insert_many(slang_documents)
 
 def load_negative_word():
     negative_word = []
@@ -39,5 +85,11 @@ def load_tweet():
         start = start + size_of_split
         max_size = max_size + size_of_split
 
-#load_tweet()
+
+
+
+load_tweet()
 load_negative_word()
+load_slang()
+load_stopwords()
+load_emojii_emoticon()
