83a6a91aa0e9699f53f529be940a4f004a511e80
Angelo <angelosansone97@gmail.com>
2020-04-20 10:17:58 +0200

    Velocizzata la memorizzazione in MongoDB

--- loadMongoDBResources.py
+++ loadMongoDBResources.py
@@ -11,15 +11,14 @@ def load_negative_word():
     with open(path_negative_word, 'r', encoding="utf8") as reader:
         line = reader.readline()
         while line != '':
-            negative_word.append(line.strip())
+            negative_word.append({"Word": line.strip()})
             line = reader.readline()
 
     for i in range(1, numberOfClusters + 1):
         print('cluster_' + str(i))
         db = client['cluster_' + str(i)]
         col = db.NegativeWord
-        for word in negative_word:
-            col.insert_one({"Word": word})
+        col.insert_many(negative_word)
 
 def load_tweet():
     tweets = []
@@ -27,7 +26,7 @@ def load_tweet():
         with open(path_tweet + '\\' + filename, 'r', encoding="utf8") as reader:
             line = reader.readline()
             while line != '':
-                tweets.append(line)
+                tweets.append({"Text": line})
                 line = reader.readline()
     start = 0
     size_of_split = int(len(tweets) / numberOfClusters)
@@ -36,10 +35,9 @@ def load_tweet():
         print('cluster_' + str(i))
         db = client['cluster_' + str(i)]
         col = db.Tweet
-        for j in range(start, max_size):
-            col.insert_one({"Text": tweets[j]})
+        col.insert_many(tweets[start:max_size])
         start = start + size_of_split
         max_size = max_size + size_of_split
 
-load_tweet()
+#load_tweet()
 load_negative_word()
